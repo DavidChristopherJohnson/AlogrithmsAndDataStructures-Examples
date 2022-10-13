fd646db77dceb924b8edddf73ab23a4be9da51e9
David <dcjohnson85@gmail.com>
2022-10-13 19:38:06 +0100

    Gave example of array data structure for Big O

---
+++ big-o/arrays.js
@@ -0,0 +1,21 @@
+const array = ["n", "n+1", "n+2"];
+
+//Actions
+
+//Accessing by index   O(1)
+//Searching            O(n)
+//Insertion - depends but max O(n)
+//Removal - depends but max O(n)
+
+//Array Methods
+
+//push                              O(1)
+//pop                               O(1)
+
+//shift                             O(n)
+//unshit                            O(n)
+//concat                            O(n)
+//slice                             O(n)
+//splice                            O(n)
+//forEach/map/filter/reduce/etc     O(n)
+//sort                              O(N * log N)
