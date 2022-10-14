0e458a4240031aff8d97ab1c098f524a3fcf8c32
David <dcjohnson85@gmail.com>
2022-10-14 20:55:16 +0100

    Minor adjustments to unique count function

--- problem-solving-patterns/count-unique-values-multiple-pointers.js
+++ problem-solving-patterns/count-unique-values-multiple-pointers.js
@@ -1,19 +1,21 @@
 const countUniqueValues = (arr) => {
+    // If array is empty then there are no values therefore no unique values
     if (arr.length === 0)
         return 0;
 
+    // If the array is only 1 value, then it is one unique value
     if (arr.length === 1)
         return 1;
 
+        // If there are only 2 values we can easily determine how many unique values
     if (arr.length === 2)
         return arr[0] === arr[1] ? 2 : 1;
 
     //Left pointer starts at index 0
     let leftPointer = 0;
-    let rightPointer;
 
     //Right pointer starts at index 1
-    for (rightPointer = 1; rightPointer < arr.length; rightPointer++) // = O(n)
+    for (let rightPointer = 1; rightPointer < arr.length; rightPointer++) // = O(n)
     {
         // If the value at the left pointer is not the same as the one at the right pointer
         // we have a new unique value, so move the left pointer right and
@@ -26,7 +28,7 @@ const countUniqueValues = (arr) => {
 
     // Remove all values after the left pointer as we have all of the unique values
     arr.splice(leftPointer + 1) // = O(n)
-    return arr;
+    return arr.length;
 }
 
 console.log(countUniqueValues([1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 5, 5]));
