a3f1eb5947a0f45a723823813c7f0bf410b3e540
David <dcjohnson85@gmail.com>
2022-10-16 19:30:02 +0100

    added sumRange as second recursive example

---
+++ recursion/sumRange.js
@@ -0,0 +1,14 @@
+// Example of a recursive function adding values to 
+// a number as it counts down
+const sumRange = (num) => {
+    // If we've reached '1' (our base case) then return it
+    if(num === 1) 
+        return 1;
+
+    // Return the current number, plus the results of 
+    // a recursive call to the sumRange function using the current number
+    // minus 1
+    return num + sumRange(num1);
+}
+
+//NOTE: Example provided from Colt Steele's course
