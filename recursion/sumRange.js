57592af3bf4c7ed0e77085aeaf6146055ba73cba
David <dcjohnson85@gmail.com>
2022-10-16 19:31:20 +0100

    Added function call and bug fix

--- recursion/sumRange.js
+++ recursion/sumRange.js
@@ -2,13 +2,15 @@
 // a number as it counts down
 const sumRange = (num) => {
     // If we've reached '1' (our base case) then return it
-    if(num === 1) 
+    if (num === 1)
         return 1;
 
     // Return the current number, plus the results of 
     // a recursive call to the sumRange function using the current number
     // minus 1
-    return num + sumRange(num1);
+    return num + sumRange(num - 1);
 }
+
+console.log(sumRange(3));
 
 //NOTE: Example provided from Colt Steele's course
