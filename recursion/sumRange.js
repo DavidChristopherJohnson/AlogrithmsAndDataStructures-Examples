30b29ba0b38215d695852c5480d1ed4c87147b70
David <dcjohnson85@gmail.com>
2022-10-16 19:32:41 +0100

    added negative safety

--- recursion/sumRange.js
+++ recursion/sumRange.js
@@ -1,9 +1,9 @@
 // Example of a recursive function adding values to 
 // a number as it counts down
 const sumRange = (num) => {
-    // If we've reached '1' (our base case) then return it
-    if (num === 1)
-        return 1;
+    // If we've reached '1' (our base case) or below then return it
+    if (num <= 1)
+        return num;
 
     // Return the current number, plus the results of 
     // a recursive call to the sumRange function using the current number
@@ -11,6 +11,6 @@ const sumRange = (num) => {
     return num + sumRange(num - 1);
 }
 
-console.log(sumRange(3));
+console.log(sumRange(-10));
 
 //NOTE: Example provided from Colt Steele's course
