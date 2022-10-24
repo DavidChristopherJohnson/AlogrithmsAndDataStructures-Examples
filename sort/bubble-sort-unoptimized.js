d7b3351ea77a650021feab2eaba2e4a1f27beab0
David <dcjohnson85@gmail.com>
2022-10-24 21:04:33 +0100

    Added more comments and a very unoptimised version of the bubble sort function

--- sort/bubble-sort-unoptimized.js
+++ sort/bubble-sort-unoptimized.js
@@ -1,6 +1,8 @@
 // This is the bubble sort example provided by Colt Steele in his course
 
 // Very Unoptimised
+// This results in an extra comparison at the end of each loop against an undefined which isn't required
+// and runs through every item in the array comparing values that have already been sorted
 const bubbleSortVeryUnoptimized = (arr) => {
     // Loop through each item to get the first value to compare
     for (let i = 0; i < arr.length; i++) {
@@ -27,6 +29,8 @@ console.log('Bubble Sort Very Unoptimized: ', result);
 
 
 // Unpotimized version of bubble sort
+// This version reduces the iteations in the inner loop as we progress to prevent
+// re-evaluating already sorted values
 const bubbleSortUnoptimized = (arr) => {
     // For each item starting at the end loop through. This is used to reduce the amount
     // of items we have to iterate over in future as the largest value will be at the 
@@ -54,6 +58,8 @@ result = bubbleSortUnoptimized([8, 1, 2, 3, 4, 5, 6, 7]);
 console.log('Bubble Sort Unoptimized: ', result);
 
 // ES2015 Version
+// This version reduces the iteations in the inner loop as we progress to prevent
+// re-evaluating already sorted values, but using new ES2015 code to perform the swap function
 const bubbleSortES2105 = (arr) => {
     // Define the swap function which will replace the value in the first defined
     // index in the passed in array with the value at the second specified index in 
